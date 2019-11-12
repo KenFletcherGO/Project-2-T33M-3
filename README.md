20170537398d489f09b819821c8d2db628ea8743
ggarnerdeng <ggarnerdeng@gmail.com>
2019-11-12 11:26:26 -0600

    Readme updated. Functions unhardcoded.

--- README.md
+++ README.md
@@ -5,17 +5,50 @@
 
     Local computer can upload/download files from local to remote repository on server computer
 
+## INSTRUCTIONS TO RUN:
+    1. Set up SSH keygen between your local and remote computer.
+            ssh.com/ssh/keygen for instructions.
+    2. Create a folder in your LOCAL user directory called "servercatchbox"
+            /home/LOCALUSER/servercatchbox
+    3. Create a folder in your REMOTE user directory called "servercatchbox"
+            /home/REMOTEUSER/servercatchbox
+    4. Go run main.go
+            follow prompts. 
+                Local User
+                Remote host IP address
+                Remote user name
+                Remote password
+    5. Add files to both servercatchbox folders and test upload/download functions.
 
+
+
+
+## DOCUMENTATION:
 11/5:   -Cloned Project 1 because similar functions can be reused for project 2
+
         -Started making function to move files from remote to local computer. 
-11/6:   -Started HTML implementation. Currently you can view the target depository of the remote                computer, but not much else. Only option 1 works, 2 and 3 do not and are just clones. 
+
+11/6:   -Started HTML implementation. Currently you can view the target depository 
+of the remote computer, but not much else. Only option 1 works, 2 and 3 do not and are just clones. 
+
         -remote computer must have a repository /home/user/servercatchbox as the directory to hold all of the files. 
+
         -HIGHLY recommend using keygen ssh to have host computer remember remote computer. 
+
 11/7:   -main page
+
             View FIles in Server
+
             Upload Files
+
             Download Files
+
 11/10:  -HTML is integrated with code. Had to rewrite the base source code. 
+
         -Upload/Download functions should be easily creatable with current version.
+
             -expect to be finished tomorrow.
+
             -need to add in a fourth user prompt to un-hardcode the local servercatchbox.
+
+11/11:  -Upload/Download functions implemented. Instructions to use app added.
--- main.go
+++ main.go
@@ -12,9 +12,12 @@ type localstruct struct {
 	FILES []string
 }
 
-var hostname, userName, password, enter string
+var hostname, userName, password, enter, localuser string
 
 func main() {
+	//These are variables for HOME/local computer:
+	fmt.Printf("Enter your user name(for local computer):  ")
+	fmt.Scanln(&localuser)
 
 	//These are login variables for the REMOTE/target computer.
 	fmt.Printf("Enter a hostname(IP) of target remote computer:  ")
@@ -67,8 +70,7 @@ func remotefiles(response http.ResponseWriter, request *http.Request) {
 func localfiles(response http.ResponseWriter, request *http.Request) {
 	temp, _ := template.ParseFiles("html/localfiles.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
-	//fmt.Println("Local TEST PASS")
-	remote, err := exec.Command("ls", "/home/garner/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
+	remote, err := exec.Command("ls", "/home/"+localuser+"/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
 	g := localstruct{FILES: make([]string, 1)}
 	length := 0
 
@@ -89,23 +91,15 @@ func localfiles(response http.ResponseWriter, request *http.Request) {
 	temp.Execute(response, g)
 }
 
-/*prototype
-func formsubmit(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("WIP", request.FormValue("transfer1"))
-}*/
-
 //prototype UPLOAD
 func uploader(response http.ResponseWriter, request *http.Request) {
 	var upload1 = request.FormValue("upload1")
 	fmt.Println("The file to upload is: " + upload1)
-	//enter = userName + "@" + hostname
+	enter = userName + "@" + hostname
 	temp, _ := template.ParseFiles("html/localfiles.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
-	//scp /home/garner/servercatchbox/localtest1 garner@192.168.1.33:/home/garner/servercatchbox ;
-	// ls /home/garner/servercatchbox > file1 ; tail file1
-	//remote, err := exec.Command("scp", "/home/garner/servercatchbox/"+upload, enter+":/home/"+userName+"/servercatchbox", ";", "ls", "/home/garner/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
-	remote, err := exec.Command("ls", "/home/garner/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
-	remote2 := exec.Command("scp", "/home/garner/servercatchbox/"+upload1, "garner@192.168.1.33:/home/garner/servercatchbox")
+	remote, err := exec.Command("ls", "/home/"+userName+"/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
+	remote2 := exec.Command("scp", "/home/"+localuser+"/servercatchbox/"+upload1, enter+":/home/"+userName+"/servercatchbox")
 
 	g := localstruct{FILES: make([]string, 1)}
 	length := 0
@@ -128,14 +122,11 @@ func uploader(response http.ResponseWriter, request *http.Request) {
 func downloader(response http.ResponseWriter, request *http.Request) {
 	var download1 = request.FormValue("download1")
 	fmt.Println("The file to download is: " + download1)
-	//enter = userName + "@" + hostname
+	enter = userName + "@" + hostname
 	temp, _ := template.ParseFiles("html/remotefiles.html")
 	response.Header().Set("Content-Type", "text/html; charset=utf-8")
-	//scp /home/garner/servercatchbox/localtest1 garner@192.168.1.33:/home/garner/servercatchbox ;
-	// ls /home/garner/servercatchbox > file1 ; tail file1
-	//remote, err := exec.Command("scp", "/home/garner/servercatchbox/"+upload, enter+":/home/"+userName+"/servercatchbox", ";", "ls", "/home/garner/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
 	remote, err := exec.Command("ssh", enter, "ls", "/home/"+userName+"/servercatchbox", ">", "file1", ";", "tail", "file1").Output()
-	remote2 := exec.Command("scp", "garner@192.168.1.33:/home/garner/servercatchbox/"+download1, "/home/garner/servercatchbox")
+	remote2 := exec.Command("scp", enter+":/home/"+userName+"/servercatchbox/"+download1, "/home/"+localuser+"/servercatchbox")
 
 	g := localstruct{FILES: make([]string, 1)}
 	length := 0
